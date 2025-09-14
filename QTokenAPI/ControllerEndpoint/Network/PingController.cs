8b1f84cd43d5548d2c9acd9f3620e0934a90c754
Prabin Rag TK <prabinrag2340@gmail.com>
2025-09-14 04:46:18 +0400

    Add PingController with GET endpoint for health check

---
+++ QTokenAPI/ControllerEndpoint/Network/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace QTokenAPI.ControllerEndpoint.Network
+{
+    [ApiController]
+    [Route("qtoken")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { Status = "Online", Timestamp = DateTime.UtcNow });
+        }
+    }
+}
